8605b530c050fbd8c62d620ac1e106c04d5008da
jourdyGiga <jourdy@gigaming.io>
2023-05-22 01:00:44 +0700

    can call go.nvim cmd like normal but the lsp key and code key are gone

--- lua/plugins/extras/lang/go.lua
+++ lua/plugins/extras/lang/go.lua
@@ -2,28 +2,42 @@ return {
   "neovim/nvim-lspconfig",
   dependencies = {
     "ray-x/go.nvim",
-    -- dependencies = { -- optional packages
-    --   "ray-x/guihua.lua",
-    --   -- "neovim/nvim-lspconfig",
-    --   -- "nvim-treesitter/nvim-treesitter",
-    -- },
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      -- "neovim/nvim-lspconfig",
+      -- "nvim-treesitter/nvim-treesitter",
+    },
 
     -- build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
 
-  event = { "CmdlineEnter" },
-  ft = { "go", "gomod" },
+  -- event = { "CmdlineEnter" },
+  -- ft = { "go", "gomod" },
 
   opts = {
     servers = {
-      gopls = {},
+      -- gopls = {},
+      gopls = {
+        cmd = { "gopls", "serve" },
+        filetypes = { "go", "gomod" },
+        -- root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+          },
+        },
+      },
+    },
 
-      setup = {
-        gopls = function(_, opts)
-          require("lazyvim.util").on_attach(function(client, buffer)
-            if client.name == "gopls" then
-              print("kabommm")
-              print("kabom", client.name)
+    setup = {
+      gopls = function(_, opts)
+        require("lazyvim.util").on_attach(function(client, buffer)
+          if client.name == "gopls" then
+            print("kabommm")
+            print("kabom", client.name)
             -- FIX: move this to go.lua in extras/lang to make it check if the file is golang to shows this key. else remove it
             -- use leader lc ? for all lauguage?
             -- for example if  the buffer is *.go make it appear as lc of all golang keys
@@ -32,29 +46,28 @@ return {
 
                 -- stylua: ignore
             vim.keymap.set("n", "<leader>cs", "<cmd>GoFillStruct<cr>", {buffer = buffer, desc = "Go Fill Struct" })
-              vim.keymap.set("n", "<leader>cf", "<cmd>GoFillSwitch<cr>", { buffer = buffer, desc = "Go Fill Switch" })
-              vim.keymap.set("n", "<leader>ct", "<cmd>GoAddTag<cr>", { buffer = buffer, desc = "Go Add Tags" })
-              vim.keymap.set("n", "<leader>cr", "<cmd>GoRmTag<cr>", { buffer = buffer, desc = "Go Remove Tags" })
+            vim.keymap.set("n", "<leader>cf", "<cmd>GoFillSwitch<cr>", { buffer = buffer, desc = "Go Fill Switch" })
+            vim.keymap.set("n", "<leader>ct", "<cmd>GoAddTag<cr>", { buffer = buffer, desc = "Go Add Tags" })
+            vim.keymap.set("n", "<leader>cr", "<cmd>GoRmTag<cr>", { buffer = buffer, desc = "Go Remove Tags" })
             -- stylua: ignore
             vim.keymap.set( "n", "<leader>cT", "<cmd>GoTestFun<cr>", { buffer = buffer, desc = "Go Test a Function" })
-              vim.keymap.set("n", "<leader>cA", "<cmd>GoTestPkg<cr>", { buffer = buffer, desc = "Go Test Package" })
+            vim.keymap.set("n", "<leader>cA", "<cmd>GoTestPkg<cr>", { buffer = buffer, desc = "Go Test Package" })
             -- stylua: ignore
             vim.keymap.set("n", "<leader>ce", "<cmd>GoIfErr<cr>", { buffer = buffer, desc = "Go Auto Generate 'if err'" })
             -- stylua: ignore
             vim.keymap.set( "n", "<leader>cc", "<cmd>GoCmt<cr>", { buffer = buffer, desc = "Go Generate Func Comments" })
-              vim.keymap.set("n", "<leader>cm", "<cmd>Gomvp<cr>", { buffer = buffer, desc = "Go Rename Module name" })
+            vim.keymap.set("n", "<leader>cm", "<cmd>Gomvp<cr>", { buffer = buffer, desc = "Go Rename Module name" })
             -- stylua: ignore
             vim.keymap.set( "n", "<leader>cm", "<cmd>GoFixPlurals<cr>", { buffer = buffer, desc = "Go Fix Redundant Func Params" }) -- not working?
             -- stylua: ignore
             vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
-              --
-              --   vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
-            end
-          end)
-          require("go").setup({ server = opts }) -- FIX:
-          return true
-        end,
-      },
+            --
+            --   vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
+          end
+        end)
+        require("go").setup({ server = opts }) -- FIX:
+        return true
+      end,
     },
   },
 }
