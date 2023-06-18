55d26c24699cb1e4767887cdcb457ae85e425ca9
jourdyGiga <jourdy@gigaming.io>
2023-06-19 00:01:03 +0700

    overwrite lua-line to change the ugly status line

---
+++ lua/plugins/lua-line.lua
@@ -0,0 +1,127 @@
+-- NOTE: transparent background for lua-line
+local my_theme = function()
+  local colors = {
+    darkgray = "#16161d",
+    gray = "#727169",
+    innerbg = nil,
+    outerbg = "#16161D",
+    normal = "#7e9cd8",
+    insert = "#98bb6c",
+    visual = "#ffa066",
+    replace = "#e46876",
+    command = "#e6c384",
+  }
+  return {
+    inactive = {
+      a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    visual = {
+      a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    replace = {
+      a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    normal = {
+      a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    insert = {
+      a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+    command = {
+      a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
+      b = { fg = colors.gray, bg = colors.outerbg },
+      c = { fg = colors.gray, bg = colors.innerbg },
+    },
+  }
+end
+
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  opts = function()
+    local icons = require("lazyvim.config").icons
+    local Util = require("lazyvim.util")
+
+    return {
+      options = {
+        theme = my_theme,
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard", "alpha" } },
+      },
+      sections = {
+        lualine_a = {
+          { "", separator = { left = "" }, right_padding = 2 },
+        },
+        lualine_b = { "" },
+        lualine_c = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = { left = "" }, padding = { left = 1, right = 0 } },
+          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+            -- stylua: ignore
+            {
+              function() return require("nvim-navic").get_location() end,
+              cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
+            },
+        },
+        lualine_x = {
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.command.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+              color = Util.fg("Statement"),
+              -- separator = { right = "" }, -- FIX: DELETE LATER
+            },
+            -- stylua: ignore
+            {
+              function() return require("noice").api.status.mode.get() end,
+              cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+              color = Util.fg("Constant"),
+            },
+            -- stylua: ignore
+            {
+              function() return "  " .. require("dap").status() end,
+              cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
+              color = Util.fg("Debug"),
+            },
+          { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = Util.fg("Special") },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+          },
+        },
+        lualine_y = {
+          { "progress", separator = { left = "" }, padding = { left = 1, right = 1 } },
+          { padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          { "location", separator = { right = "" }, left_padding = 1 },
+        },
+      },
+      extensions = { "neo-tree", "lazy" },
+    }
+  end,
+}
