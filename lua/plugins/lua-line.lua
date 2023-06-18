8e6b4612cafa0e3fd71c765d5f078e629b3cd2f5
jourdyGiga <jourdy@gigaming.io>
2023-06-19 00:17:34 +0700

    add seperator

--- lua/plugins/lua-line.lua
+++ lua/plugins/lua-line.lua
@@ -74,6 +74,7 @@ return {
               info = icons.diagnostics.Info,
               hint = icons.diagnostics.Hint,
             },
+            separator = { left = "" },
           },
           { "filetype", icon_only = true, separator = { left = "" }, padding = { left = 1, right = 0 } },
           { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
