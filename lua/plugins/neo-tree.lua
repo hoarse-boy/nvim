89302b7f7efa213d2c201bce19871782736509ce
jourdyGiga <jourdy@gigaming.io>
2023-07-02 21:55:04 +0700

    dont use nvim tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,7 +1,6 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    -- disable = true, -- FIX: DELETE LATER teting nvim tree
     opts = {
       -- FIX: NOT WORKING
       -- event_handlers = {
@@ -64,22 +63,3 @@ return {
     },
   },
 }
-
--- require('neo-tree').setup({
---   event_handlers = {
---     {
---       event = "neo_tree_buffer_enter",
---       handler = function()
---         -- This effectively hides the cursor
---         vim.cmd 'highlight! Cursor blend=100'
---       end
---     },
---     {
---       event = "neo_tree_buffer_leave",
---       handler = function()
---         -- Make this whatever your current Cursor highlight group is.
---         vim.cmd 'highlight! Cursor guibg=#5f87af blend=0'
---       end
---     }
---   },
--- })
