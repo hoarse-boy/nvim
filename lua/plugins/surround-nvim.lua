8d6f24fd614dbf2c522bd1899c5fde61d31cd594
jourdyGiga <jourdy@gigaming.io>
2023-07-02 08:49:34 +0700

    update nvim surround keymaps

--- lua/plugins/surround-nvim.lua
+++ lua/plugins/surround-nvim.lua
@@ -1,11 +1,30 @@
 return {
   "kylechui/nvim-surround",
   version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  disable = true,
   event = "VeryLazy",
-  config = function()
+  config = function(_, opts)
     require("nvim-surround").setup({
-      -- Configuration here, or leave empty to use defaults
+      keymaps = {
+        insert = "<C-g>s",
+        insert_line = "<C-g>S",
+        normal = "ys",
+        normal_cur = "yss",
+        normal_line = "yS",
+        normal_cur_line = "ySS",
+        visual = "gsa",
+        visual_line = "gS",
+        delete = "gsd",
+        change = "gsc",
+        change_line = "gcS",
+      },
     })
+
+    local wk = require("which-key")
+    wk.setup(opts)
+    local keymaps = {
+      mode = { "n", "v" },
+      ["gs"] = { name = "+surround" },
+    }
+    wk.register(keymaps, opts)
   end,
 }
