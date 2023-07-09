b51b17b89fab6323cd509ea91a2501f209b3e85e
jourdyGiga <jourdy@gigaming.io>
2023-07-09 17:51:46 +0700

    update themes

--- lua/plugins/themes/init.lua
+++ lua/plugins/themes/init.lua
@@ -1,11 +1,11 @@
 return {
   { require("plugins.themes.kanagawa") },
-  -- { require("plugins.themes.starry") },
-  -- { require("plugins.themes.aurora") },
-  -- { require("plugins.themes.catppuccin") },
-  -- { require("plugins.themes.tokyo-night") },
-  -- { require("plugins.themes.nightfox") },
-  -- { require("plugins.themes.vscode") },
+  { require("plugins.themes.starry") },
+  { require("plugins.themes.aurora") },
+  { require("plugins.themes.catppuccin") },
+  { require("plugins.themes.tokyo-night") },
+  { require("plugins.themes.nightfox") },
+  { require("plugins.themes.vscode") },
   -- { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load a theme
