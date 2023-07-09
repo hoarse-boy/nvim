49265c44094b5cd9d55d77d61fb0aae91471af5a
jourdyGiga <jourdy@gigaming.io>
2023-07-09 17:43:30 +0700

    move themes.lua to folder themes's init.lua

--- lua/plugins/themes/kanagawa.lua
+++ lua/plugins/themes/kanagawa.lua
@@ -82,6 +82,8 @@ local myColor = {
 return {
   "rebelot/kanagawa.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  -- lazy = true,
+  name = "kanagawa",
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require("kanagawa").setup({
