7601c6030ed8bfae9db968e83595a16f3460036c
jourdyGiga <jourdy@gigaming.io>
2023-07-18 16:55:57 +0700

    commented flash, as it is now the default

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,7 +31,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" }, -- NOTE: use cinnamon.nvim. better in all regards. can animate any motion even gg and { and }
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
+    -- { import = "lazyvim.plugins.extras.editor.flash" }, -- flash has became the default for lazyvim
     -- { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     -- import/override with your plugins
