f50d097a339b368ef91ccd964019f64812d5804d
jourdyGiga <jourdy@gigaming.io>
2023-07-19 22:52:57 +0700

    disable ts context and added ts playground

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -4,8 +4,60 @@ return {
     dependencies = {
       { "p00f/nvim-ts-rainbow" }, -- the original rainbow still works fine
 
+      -- treesitter playground
+      {
+        "nvim-treesitter/playground",
+        config = function()
+          require("nvim-treesitter.configs").setup({
+            playground = {
+              enable = true,
+              disable = {},
+              updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+              persist_queries = false, -- Whether the query persists across vim sessions
+              keybindings = {
+                toggle_query_editor = "o",
+                toggle_hl_groups = "i",
+                toggle_injected_languages = "t",
+                toggle_anonymous_nodes = "a",
+                toggle_language_display = "I",
+                focus_language = "f",
+                unfocus_language = "F",
+                update = "R",
+                goto_node = "<cr>",
+                show_help = "?",
+              },
+            },
+          })
+        end,
+        keys = {
+          {
+            "<leader>bt",
+            function()
+              vim.cmd("TSPlaygroundToggle")
+            end,
+            desc = "Toggle Treesitter Playground",
+          },
+          {
+            "<leader>bh",
+            function()
+              vim.cmd("TSHighlightCapturesUnderCursor")
+            end,
+            desc = "Toggle Highlight Capture",
+          },
+        },
+      },
+      {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+          if type(opts.ensure_installed) == "table" then
+            vim.list_extend(opts.ensure_installed, { "query" })
+          end
+        end,
+      },
+
       {
         "nvim-treesitter/nvim-treesitter-context",
+        enabled = false, -- FIX: it causes an error when using {} to move
         config = function()
           require("treesitter-context").setup({
             enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
