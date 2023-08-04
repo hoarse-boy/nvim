c8d954fc098a83721aebc94b88ca7a91f48f4e46
jourdyGiga <jourdy@gigaming.io>
2023-08-04 11:32:35 +0700

    build(lazy): update plugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,7 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- NOTE: configure the speed of yank
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
