c2659257690492846406bd65e84bc12ea1f7283c
jourdyGiga <jourdy@gigaming.io>
2023-08-06 20:07:54 +0700

    refactor(json5): still cannot compile json5 rust code so it is commented for now

--- lua/plugins/extras/dap/dap.lua
+++ lua/plugins/extras/dap/dap.lua
@@ -8,9 +8,17 @@ return {
           require("dap-go").setup()
         end,
       },
+      -- TODO: find out how to install and compile it https://github.com/Joakker/lua-json5
+      -- FIX: fail to build. find out why
+      -- https://github.com/folke/lazy.nvim/issues/368
+      -- require('dap.ext.vscode').json_decode = require'json5'.parse
+      -- {
+      --   "Joakker/lua-json5",
+      --   build = "./install.sh",
+      -- },
     },
     opts = function()
-      -- PERF: enabling nvim to load .vscode/launch.json file for debuging
+      -- NOTE: enabling nvim to load .vscode/launch.json file for debuging
       -- add rt_lldb for debuging rust using plugin rust-tools
       require("dap.ext.vscode").load_launchjs(nil, { rt_lldb = { "rust" } })
     end,
