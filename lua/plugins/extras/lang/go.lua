08f435118d3b8ef49a0a2ce17055213f14a392f9
jourdyGiga <jourdy@gigaming.io>
2023-09-11 10:33:51 +0700

    feat(hydrate): add new plugin

--- lua/plugins/extras/lang/go.lua
+++ lua/plugins/extras/lang/go.lua
@@ -116,6 +116,7 @@ return {
     opts = {
       servers = {
         gopls = {
+          root_dir = require("lspconfig.util").root_pattern("neo-tree", "alpha"), -- NOTE: this will enable LspStart gopls when in neo-tree buffer
           settings = {
             gopls = {
               gofumpt = false,
