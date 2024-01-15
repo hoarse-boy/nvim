58e328f0b39d6c5eea567effd5cdec819bed1135
jourdy-mago <jourdymagoofficial@gmail.com>
2024-01-15 19:04:04 +0700

    fix: fix the glitchy animation when using mini.animate by holding the movement button

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -58,91 +58,41 @@ return {
     version = false,
     -- enabled = false, -- disabled plugin
     event = "VeryLazy",
-    keys = {
-      -- { "<leader>l", "", desc = "+go.nvim" }, -- example
-      -- { "<leader>ls", "<cmd>GoFillStruct<cr>", desc = "Fill Struct" }, -- example
-      { "define keymaps", "what the keys do", desc = "description" },
-    },
-    config = function(_, opts)
+    config = function(_, _)
       if not vim.g.neovide then
         local animate = require("mini.animate")
 
         animate.setup({
           -- Cursor path
           cursor = {
-            enable = true,
-            -- Timing of animation (how steps will progress in time)
-            -- timing = --<function: implements linear total 250ms animation duration>,
-            -- Path generator for visualized cursor movement
-            -- path = --<function: implements shortest line path>,
+            enable = false,
+            timing = animate.gen_timing.linear({ duration = 20, unit = "total" }),
           },
 
           -- Vertical scroll
           scroll = {
             enable = true,
-            -- timing = animate.gen_timing.cubic({ duration = 20, unit = "total" }),
             timing = animate.gen_timing.linear({ duration = 10, unit = "total" }),
-
             -- Animate equally but with at most 120 steps instead of default 60
-            subscroll = animate.gen_subscroll.equal({ max_output_steps = 10 }),
+            subscroll = animate.gen_subscroll.equal({ max_output_steps = 25 }),
             -- subscroll = animate.gen_subscroll.equal({ max_output_steps = 120 }),
           },
 
           -- Window resize
           resize = {
-            -- Whether to enable this animation
-            enable = disable,
-
-            -- Timing of animation (how steps will progress in time)
-            -- timing = --<function: implements linear total 250ms animation duration>,
-
-            -- Subresize generator for all steps of resize animations
-            -- subresize = --<function: implements equal linear steps>,
+            enable = false,
           },
 
           -- Window open
           open = {
             enable = false,
-
-            -- Timing of animation (how steps will progress in time)
-            -- timing = --<function: implements linear total 250ms animation duration>,
-
-            -- Floating window config generator visualizing specific window
-            -- winconfig = --<function: implements static window for 25 steps>,
-
-            -- 'winblend' (window transparency) generator for floating window
-            -- winblend = --<function: implements equal linear steps from 80 to 100>,
           },
 
           -- Window close
           close = {
-            -- Whether to enable this animation
             enable = false, -- NOTE: disable this to remove the ugly black bg hl when closing neo-tree
-
-            -- Timing of animation (how steps will progress in time)
-            -- timing = --<function: implements linear total 250ms animation duration>,
-
-            -- Floating window config generator visualizing specific window
-            -- winconfig = --<function: implements static window for 25 steps>,
-
-            -- 'winblend' (window transparency) generator for floating window
-            -- winblend = --<function: implements equal linear steps from 80 to 100>,
           },
         })
-
-        -- TODO: current issue is when press and hold c-d or c-u it will have strange animation.
-        -- find the cause and fix / report it.
-
-        -- vim.keymap.set(
-        --   "n",
-        --   "<C-d>",
-        --   [[<Cmd>lua vim.cmd('normal! <C-d>'); MiniAnimate.execute_after('scroll', 'normal! zvzz')<CR>]]
-        -- )
-        -- vim.keymap.set(
-        --   "n",
-        --   "<C-u>",
-        --   [[<Cmd>lua vim.cmd('normal! <C-u>'); MiniAnimate.execute_after('scroll', 'normal! zvzz')<CR>]]
-        -- )
       end
     end,
   },
