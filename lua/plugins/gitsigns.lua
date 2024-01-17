dc944ed023e1479d6bb3ffaef21d5d9d6d19666c
jourdyGiga <jourdy@gigaming.io>
2024-01-17 22:03:06 +0700

    fix: the delay when using git blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,9 +1,22 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = "LazyFile",
-  keys = {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "LazyFile",
+    keys = {
     -- stylua: ignore
     -- there is duplicate keymap to toggle git blame as the other is too deep inside a another prefix 'h'.
     { "<leader>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "Toggle Git Blame (virtual text)", mode = "n"  },
+    },
+    opts = function(_, opts)
+      -- change the default delay to 0.
+      opts.current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 0,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        -- current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+      }
+    end, -- use this to not overwrite this plugin config (usefull in lazyvim)
   },
 }
