26aaf9ab072e1abbc855ccad83a35286eb34e729
jourdyGiga <jourdy@gigaming.io>
2024-02-29 15:28:10 +0700

    fix: disable auto format when saving
    manually format using whickey

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -111,7 +111,9 @@ end
 opt.list = false -- NOTE: make the > and other symbol to be hidden when the object is commented.
 opt.scrolloff = 10 -- minimal number of screen lines to keep above and below the cursor.
 opt.sidescrolloff = 10 -- minimal number of screen lines to keep left and right of the cursor.
-opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-opt.swapfile = false -- creates a swapfile. disable to avoid the annoying prompt
-opt.hlsearch = true -- highlight all matches on previous search pattern
+opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited.
+opt.swapfile = false -- creates a swapfile. disable to avoid the annoying prompt.
+opt.hlsearch = true -- highlight all matches on previous search pattern.
 opt.relativenumber = false
+
+vim.g.autoformat = false -- disable auto format. use <leader>cf to format.
