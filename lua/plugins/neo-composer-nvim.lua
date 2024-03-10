2ad10f8f7b824f0405d9dde175079754f28f49e7
jourdyGiga <jourdy@gigaming.io>
2024-03-10 14:34:28 +0700

    style: fmt lua code

--- lua/plugins/neo-composer-nvim.lua
+++ lua/plugins/neo-composer-nvim.lua
@@ -1,12 +1,24 @@
 return {
-  "ecthelionvi/NeoComposer.nvim",
-  event = "VeryLazy",
-  keys = {
-    { "<leader>m", "<cmd>Telescope macros<cr>", desc = "NeoComposer (macro)" },
+  {
+    "ecthelionvi/NeoComposer.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>m", "<cmd>Telescope macros<cr>", desc = "NeoComposer (macro)" },
+    },
+    dependencies = { "kkharji/sqlite.lua", "nvim-telescope/telescope.nvim" },
+    config = function(_, opts)
+      require("NeoComposer").setup()
+      require("telescope").load_extension("macros")
+    end,
   },
-  dependencies = { "kkharji/sqlite.lua", "nvim-telescope/telescope.nvim" },
-  config = function(_, opts)
-    require("NeoComposer").setup()
-    require("telescope").load_extension("macros")
-  end,
+
+  -- FIX: not working.
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, 2, require("NeoComposer.ui").status_recording)
+    end,
+  },
 }
