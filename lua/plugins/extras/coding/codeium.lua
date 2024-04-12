dad3b6365df7fb884db98478b632a7e534f31f28
jourdyGiga <jourdy@gigaming.io>
2024-04-12 08:18:44 +0700

    docs: add comment

--- lua/plugins/extras/coding/codeium.lua
+++ lua/plugins/extras/coding/codeium.lua
@@ -1,5 +1,6 @@
 return {
   -- NOTE: will change to use the codeium.nvim below if the issue is solved.
+  -- it also supports chat now but still waiting PR to fix the root dir bug where it doesnt know the root dir.
   {
     {
       "Exafunction/codeium.vim",
@@ -55,4 +56,35 @@ return {
   --     table.insert(opts.sections.lualine_x, 2, require("lazyvim.util").lualine.cmp_source("codeium"))
   --   end,
   -- },
+
+  -- {
+  --   "nvim-cmp",
+  --   dependencies = {
+  --     -- codeium
+  --     {
+  --       "Exafunction/codeium.nvim",
+  --       cmd = "Codeium",
+  --       build = ":Codeium Auth",
+  --       opts = {},
+  --     },
+  --   },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, 1, {
+  --       name = "codeium",
+  --       group_index = 1,
+  --       priority = 100,
+  --     })
+  --   end,
+  -- },
+
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   optional = true,
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("codeium"))
+  --   end,
+  -- },
+
 }
