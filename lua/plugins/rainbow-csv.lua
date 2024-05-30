53c2a1319bbfc80d62d9fd95ff12579246d66da4
jourdyGiga <jourdy@gigaming.io>
2024-05-30 14:19:18 +0700

    feat: add new plugins

--- lua/plugins/rainbow-csv.lua
+++ lua/plugins/rainbow-csv.lua
@@ -1,20 +1,37 @@
 return {
-  "cameron-wags/rainbow_csv.nvim",
-  lazy = "VeryLazy",
-  config = true,
-  ft = {
-    "csv",
-    "tsv",
-    "csv_semicolon",
-    "csv_whitespace",
-    "csv_pipe",
-    "rfc_csv",
-    "rfc_semicolon",
+  {
+    "cameron-wags/rainbow_csv.nvim",
+    event = "VeryLazy",
+    config = true,
+    ft = {
+      "csv",
+      "tsv",
+      "csv_semicolon",
+      "csv_whitespace",
+      "csv_pipe",
+      "rfc_csv",
+      "rfc_semicolon",
+    },
+    cmd = {
+      "RainbowDelim",
+      "RainbowDelimSimple",
+      "RainbowDelimQuoted",
+      "RainbowMultiDelim",
+    },
   },
-  cmd = {
-    "RainbowDelim",
-    "RainbowDelimSimple",
-    "RainbowDelimQuoted",
-    "RainbowMultiDelim",
-  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "csv" })
+      end
+
+      -- disable csv highlighting as the treesitter has auto install. use the rainbow_csv.nvim highlighting instead.
+      if type(opts.highlight.disable) == "table" then
+        vim.list_extend(opts.highlight.disable, { "csv" })
+      else
+        opts.highlight.disable = { "csv" }
+      end
+    end,
+  }
 }
