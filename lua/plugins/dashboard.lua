c1b11b693ec56afb925925858005a87a24e88b81
hoarse-boy <jourdymagoofficial@gmail.com>
2024-06-18 16:00:59 +0700

    refactor: dashboard to append, modify, and remove instead of creating new instance of dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,13 +1,4 @@
-return {
-  "nvimdev/dashboard-nvim",
-  keys = {
-    { "<leader>D", "<cmd>Dashboard<cr>", desc = "Dashboard" }, -- example
-  },
-  opts = function(_, opts)
-    -- NOTE: create the braille at https://asciiart.club/
-    -- https://superemotes.com/img2ascii#google_vignette -- just copy the text.
-
-    local logo = [[
+local logo = [[
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣶⣶⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠿⠿⠿⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -25,22 +16,76 @@ return {
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ]]
 
+
+-- modify existing dashboard shortcut in config.center. the one showing when launching nvim.
+local function update_dashboard_shortcut(opts, current_keymap, new_action, new_desc)
+  for _, item in ipairs(opts.config.center) do
+    if item.key == current_keymap then
+      item.action = new_action
+      item.desc = new_desc
+    end
+  end
+end
+
+local function remove_dashboard_item(opts, key_to_remove)
+  for index, item in ipairs(opts.config.center) do
+    if item.key == key_to_remove then
+      table.remove(opts.config.center, index)
+      break
+    end
+  end
+end
+
+return {
+  "nvimdev/dashboard-nvim",
+  keys = {
+    { "<leader>D", "<cmd>Dashboard<cr>", desc = "Dashboard" }, -- example
+  },
+  opts = function(_, opts)
+    -- NOTE: create the braille at https://asciiart.club/
+    -- https://superemotes.com/img2ascii#google_vignette -- just copy the text.
+
     logo = string.rep("\n", 8) .. logo .. "\n\n"
     opts.config.header = vim.split(logo, "\n")
 
-    -- NOTE: tips, use [[lua func / vim command]] if require directly, dashboard will execute that func.
-    -- the require("plugins.util.teles-find") purpose is to cd first to the target dir. by doing so, the nerd-tree will show and focus only the dir. it will also combat the dir changes when going deep into nested folder.
-    opts.config.center = {
-      { action = LazyVim.telescope("files"), desc = " Find File", icon = " ", key = "f" },
-      { action = [[lua require("plugins.util.teles-find").ChangeDirAndFindFiles("~/google-drive/obsidian-vault/todos/")]], desc = " Obsidian Todos", icon = " ", key = "t" },
-      { action = [[lua require("plugins.util.teles-find").ChangeDirAndFindFiles("~/google-drive/obsidian-vault/inbox/")]], desc = " Obsidian Inbox", icon = "󱉳 ", key = "i" },
-      { action = "Telescope oldfiles", desc = " Recent Files", icon = " ", key = "r" },
-      { action = "Telescope live_grep", desc = " Find Text", icon = " ", key = "g" },
-      { action = [[lua require("plugins.util.teles-find").ChangeDirAndFindFiles("~/.config/nvim//")]], desc = " Config", icon = " ", key = "c" },
-      { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-      { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
-      { action = "qa", desc = " Quit", icon = " ", key = "q" },
+    -- TODO: update obsidian_todos and obsidian_inbox to have dynamic dir using util function. for macos and arch linux.
+    local obsidian_todos = {
+      action = [[lua require("plugins.util.teles-find").ChangeDirAndFindFiles("~/google-drive/obsidian-vault/todos/")]],
+      desc = " Obsidian Todos",
+      icon = " ",
+      key = "t"
     }
+
+    obsidian_todos.desc = obsidian_todos.desc .. string.rep(" ", 43 - #obsidian_todos.desc)
+    obsidian_todos.key_format = "  %s"
+
+    table.insert(opts.config.center, 2, obsidian_todos)
+
+    local obsidian_inbox = {
+      action = [[lua require("plugins.util.teles-find").ChangeDirAndFindFiles("~/google-drive/obsidian-vault/inbox/")]],
+      desc = " Obsidian Inbox", icon = "󱉳 ", key = "i"
+    }
+
+    obsidian_inbox.desc = obsidian_inbox.desc .. string.rep(" ", 43 - #obsidian_inbox.desc)
+    obsidian_inbox.key_format = "  %s"
+
+    table.insert(opts.config.center, 3, obsidian_inbox)
+
+    local lazyvim_config = {
+      action = [[lua require("plugins.util.teles-find").ChangeDirAndFindFiles("~/.local/share/nvim/lazy/LazyVim/")]],
+      desc = " Lazyvim Config", icon = " ", key = "L"
+    }
+
+    lazyvim_config.desc = lazyvim_config.desc .. string.rep(" ", 43 - #lazyvim_config.desc)
+    lazyvim_config.key_format = "  %s"
+
+    table.insert(opts.config.center, 11, lazyvim_config)
+
+    update_dashboard_shortcut(opts, "c", [[lua require("plugins.util.teles-find").ChangeDirAndFindFiles("~/.config/nvim/")]], " Config")
+
+    -- remove some defaults dashboard items.
+remove_dashboard_item(opts, "n")
+remove_dashboard_item(opts, "x")
   end,
 }
 
