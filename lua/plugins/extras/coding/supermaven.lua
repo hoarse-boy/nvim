533a5118b0813b05d47ddeeb3b662f6cce65e779
hoarse-boy <jourdymagoofficial@gmail.com>
2024-06-20 16:06:55 +0700

    fix: a temp fix for issue #51 in supermaven github

--- lua/plugins/extras/coding/supermaven.lua
+++ lua/plugins/extras/coding/supermaven.lua
@@ -1,4 +1,4 @@
-return{
+return {
   {
     "supermaven-inc/supermaven-nvim",
     event = "VeryLazy",
@@ -7,17 +7,22 @@ return{
         keymaps = {
           accept_suggestion = "<C-a>",
           clear_suggestion = "<C-]>",
-          accept_word = "<C-l>",
+          accept_word = "<C-n>",
         },
         -- ignore_filetypes = { cpp = true },
         color = {
-          suggestion_color = "#cfcccc",
-          cterm = 244,
+          -- suggestion_color = "#cfcccc",
+          -- cterm = 244,
+          -- -- NOTE: temp fix. https://github.com/supermaven-inc/supermaven-nvim/pull/53
+          suggestion_color = vim.api.nvim_get_hl(0, { name = "NonText" }).fg,
+          cterm = vim.api.nvim_get_hl(0, { name = "NonText" }).cterm,
         },
-        log_level = "info", -- set to "off" to disable logging completely
+        log_level = "info",                -- set to "off" to disable logging completely
         disable_inline_completion = false, -- disables inline completion for use with cmp
-        disable_keymaps = false, -- disables built in keymaps for more manual control
+        disable_keymaps = false,           -- disables built in keymaps for more manual control
       })
+      require("supermaven-nvim.completion_preview").suggestion_group = "SupermavenSuggestion"
     end,
-  }
+  },
+  {},
 }
