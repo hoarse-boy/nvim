78ac10b207e51164eb6ca665f71944f66822f629
jourdyGiga <jourdy@gigaming.io>
2024-06-21 23:21:46 +0700

    fix: checkealth shwoing warning
    also remove a keybind as it is deprecated

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,6 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf") -- use fzf
-      end,
-    },
-
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.defaults.file_ignore_patterns, { "node_modules", "vendor", "proto/*", "**/*.pb.go" }) -- NOTE: ignore folder / files for live grep
@@ -24,14 +16,16 @@ return {
       -- { "<leader>xT", false },
       -- change a keymap
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-      -- add a keymap to browse plugin files
-      {
-        "<leader>fP",
-        function()
-          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-        end,
-        desc = "Find Plugin File",
-      },
+      -- add a keymap to browse plugin files. NOTE: this is not needed as the dashboard has handled it.
+      -- {
+      --   "<leader>fP",
+      --   function()
+      --     require("telescope.builtin").find_files({
+      --       cwd = require("lazy.core.config").options.root,
+      --     })
+      --   end,
+      --   desc = "Find Plugin File",
+      -- },
     },
   },
 }
