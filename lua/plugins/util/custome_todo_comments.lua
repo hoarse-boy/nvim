bd79073fd70ee0653fa96865e4523958ce1f1038
jourdyGiga <jourdy@gigaming.io>
2024-06-22 19:12:35 +0700

    refactor: update the func to be dynamic and change file and function name

---
+++ lua/plugins/util/custome_todo_comments.lua
@@ -0,0 +1,67 @@
+local M = {}
+
+-- define a local function to insert a todo comment keyword line and enter insert mode.
+function M.insert_custom_todo_comments(keyword_str)
+  if keyword_str == nil then
+    keyword_str = "MARKED: " -- default.
+  end
+
+  -- Get the current buffer number
+  local bufnr = vim.api.nvim_get_current_buf()
+  -- Get the current line number
+  local linenr = vim.api.nvim_win_get_cursor(0)[1]
+  -- Get the file type of the current buffer using vim.bo
+  local filetype = vim.bo[bufnr].filetype
+
+  -- Define comment symbols for various filetypes
+  local filetype_formats = {
+    javascript = "// %s",
+    typescript = "// %s",
+    java = "// %s",
+    c = "// %s",
+    go = "// %s",
+    rust = "// %s",
+    php = "// %s",
+    cpp = "// %s",
+    sh = "# %s",
+    bash = "# %s",
+    perl = "# %s",
+    ruby = "# %s",
+    python = "# %s",
+    mojo = "# %s",
+    html = "<!-- %s -->",
+    markdown = "<!-- %s -->",
+    lua = "-- %s",
+    css = "/* %s */",
+  }
+
+  -- Get the comment symbol for the current filetype
+  local comment_symbol = filetype_formats[filetype]
+
+  -- Handle cases where the file type isn't in the list
+  if comment_symbol == nil then
+    print("Filetype " .. filetype .. " is not supported. manually add it to the comment_symbols in insert_custom_todo_comments.lua in util folder.")
+    comment_symbol = "%s"
+  end
+
+  -- Format the commented string
+  local fmt_keyword = string.format(comment_symbol, keyword_str)
+
+  -- Check if the current line is empty before inserting the custom keyword line
+  local current_line = vim.api.nvim_buf_get_lines(bufnr, linenr - 1, linenr, false)[1]
+  if current_line == nil or current_line:match("^%s*$") then
+    -- If current line is empty, insert the custom keyword line directly
+    vim.api.nvim_buf_set_lines(bufnr, linenr - 1, linenr, false, { fmt_keyword })
+  else
+    -- If current line is not empty, insert the custom keyword line above and adjust current line position
+    vim.api.nvim_buf_set_lines(bufnr, linenr - 1, linenr - 1, false, { fmt_keyword })
+  end
+
+  -- Move cursor to the end of the custom keyword line and enter insert mode
+  vim.api.nvim_win_set_cursor(0, { linenr, #fmt_keyword })
+
+  -- Enter insert mode
+  vim.cmd("startinsert!")
+end
+
+return M
