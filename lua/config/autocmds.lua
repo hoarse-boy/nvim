0f8038753faf021401b3bb798078de4f7efd70b0
jourdyGiga <jourdy@gigaming.io>
2024-08-13 14:45:18 +0700

    refactor: change back the behavour of auto comment

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -21,18 +21,19 @@ augroup("mygroup", { clear = true })
 --   end,
 -- })
 
-autocmd("Filetype", {
-  pattern = { "*" },
-  callback = function()
-    -- vim.opt.formatoptions = vim.opt.formatoptions - "o"
-    if vim.bo["ft"] == "css" then
-      vim.opt_local.formatoptions:remove("r") -- don't enter comment leader on Enter in css files
-    end
-    vim.opt.formatoptions = vim.opt.formatoptions + {
-      o = false, -- Don't continue comments with o and O
-      r = false, -- don't insert comment leader on Enter
-    }
-  end,
-  group = "mygroup",
-  desc = "Don't continue comments with o, O, and enter",
-})
+-- disable auto add comment to a new line.
+-- autocmd("Filetype", {
+--   pattern = { "*" },
+--   callback = function()
+--     -- vim.opt.formatoptions = vim.opt.formatoptions - "o"
+--     if vim.bo["ft"] == "css" then
+--       vim.opt_local.formatoptions:remove("r") -- don't enter comment leader on Enter in css files
+--     end
+--     vim.opt.formatoptions = vim.opt.formatoptions + {
+--       o = false, -- Don't continue comments with o and O
+--       r = false, -- don't insert comment leader on Enter
+--     }
+--   end,
+--   group = "mygroup",
+--   desc = "Don't continue comments with o, O, and enter",
+-- })
