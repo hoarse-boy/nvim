ddcf149437e9ccd694bad1ae39b4e433c3396c12
jourdyGiga <jourdy@gigaming.io>
2024-08-27 01:28:17 +0700

    fix: add bool to make it append mark to be in insert mode if true

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,9 +1,9 @@
 local printf = require("plugins.util.printf").printf
 
+-- other keymaps for marking buffer are here.
 return {
   "folke/todo-comments.nvim",
   opts = function(_, opts)
-
     opts.merge_keywords = true -- when true, custom keywords will be merged with the defaults
 
     -- add my custom todo.
@@ -31,13 +31,16 @@ return {
     {
       "<leader>mf", -- TODO: change the insert_custom_todo_comments func to not add new line but add to the right as comments? if lang is not availabl, create new line.
       function()
-        require("plugins.util.custom_todo_comments").append_todo_comments_to_current_line()
+        require("plugins.util.custom_todo_comments").append_todo_comments_to_current_line("FIX: ", true)
       end,
       mode = "n",
       desc = printf("Insert FIXED todo"),
       noremap = true,
       silent = true,
     },
+    -- TODO: change todo telescopt and trouble.
+    -- for example make 'mt' to be regular tele to search MARKED.
+    -- but make mT to be telescopte to show selection first such as 'MARKED' and then 'FIXED' and search using telescope.
     {
       "<leader>mt",
       "<cmd>TodoTelescope keywords=MARKED<cr>",
--- lua/plugins/util/custom_todo_comments.lua
+++ lua/plugins/util/custom_todo_comments.lua
@@ -1,5 +1,27 @@
 local M = {}
 
+-- Define comment symbols for various filetypes
+local filetype_formats = {
+  javascript = "// %s",
+  typescript = "// %s",
+  java = "// %s",
+  c = "// %s",
+  go = "// %s",
+  rust = "// %s",
+  php = "// %s",
+  cpp = "// %s",
+  sh = "# %s",
+  bash = "# %s",
+  perl = "# %s",
+  ruby = "# %s",
+  python = "# %s",
+  mojo = "# %s",
+  html = "<!-- %s -->",
+  markdown = "<!-- %s -->",
+  lua = "-- %s",
+  css = "/* %s */",
+}
+
 -- define a local function to insert a todo comment keyword line and enter insert mode.
 function M.insert_custom_todo_comments(keyword_str)
   if keyword_str == nil then
@@ -13,34 +35,14 @@ function M.insert_custom_todo_comments(keyword_str)
   -- Get the file type of the current buffer using vim.bo
   local filetype = vim.bo[bufnr].filetype
 
-  -- Define comment symbols for various filetypes
-  local filetype_formats = {
-    javascript = "// %s",
-    typescript = "// %s",
-    java = "// %s",
-    c = "// %s",
-    go = "// %s",
-    rust = "// %s",
-    php = "// %s",
-    cpp = "// %s",
-    sh = "# %s",
-    bash = "# %s",
-    perl = "# %s",
-    ruby = "# %s",
-    python = "# %s",
-    mojo = "# %s",
-    html = "<!-- %s -->",
-    markdown = "<!-- %s -->",
-    lua = "-- %s",
-    css = "/* %s */",
-  }
-
   -- Get the comment symbol for the current filetype
   local comment_symbol = filetype_formats[filetype]
 
   -- Handle cases where the file type isn't in the list
   if comment_symbol == nil then
-    print("Filetype " .. filetype .. " is not supported. manually add it to the comment_symbols in insert_custom_todo_comments.lua in util folder.")
+    print("Filetype " ..
+    filetype ..
+    " is not supported. manually add it to the comment_symbols in insert_custom_todo_comments.lua in util folder.")
     comment_symbol = "%s"
   end
 
@@ -66,7 +68,7 @@ end
 
 -- TODO: add  bool in params, if true, go to insert mode by using vim 'A'.
 -- define a local function to insert a todo comment keyword at the end of the current line.
-function M.append_todo_comments_to_current_line(keyword_str)
+function M.append_todo_comments_to_current_line(keyword_str, is_entering_insert_mode)
   if keyword_str == nil then
     keyword_str = "FIX: " -- default.
   end
@@ -78,34 +80,14 @@ function M.append_todo_comments_to_current_line(keyword_str)
   -- Get the file type of the current buffer using vim.bo
   local filetype = vim.bo[bufnr].filetype
 
-  -- Define comment symbols for various filetypes
-  local filetype_formats = {
-    javascript = "// %s",
-    typescript = "// %s",
-    java = "// %s",
-    c = "// %s",
-    go = "// %s",
-    rust = "// %s",
-    php = "// %s",
-    cpp = "// %s",
-    sh = "# %s",
-    bash = "# %s",
-    perl = "# %s",
-    ruby = "# %s",
-    python = "# %s",
-    mojo = "# %s",
-    html = "<!-- %s -->",
-    markdown = "<!-- %s -->",
-    lua = "-- %s",
-    css = "/* %s */",
-  }
-
   -- Get the comment symbol for the current filetype
   local comment_symbol = filetype_formats[filetype]
 
   -- Handle cases where the file type isn't in the list
   if comment_symbol == nil then
-    print("Filetype " .. filetype .. " is not supported. manually add it to the comment_symbols in insert_custom_todo_comments.lua in util folder.")
+    print("Filetype " ..
+    filetype ..
+    " is not supported. manually add it to the comment_symbols in insert_custom_todo_comments.lua in util folder.")
     comment_symbol = "%s"
   end
 
@@ -120,6 +102,15 @@ function M.append_todo_comments_to_current_line(keyword_str)
 
   -- Update the current line with the new content
   vim.api.nvim_buf_set_lines(bufnr, linenr - 1, linenr, false, { new_line })
+
+  -- FIX: test
+  if is_entering_insert_mode then
+    -- Move cursor to the end of the custom keyword line and enter insert mode
+    vim.api.nvim_win_set_cursor(0, { linenr, #fmt_keyword })
+
+    -- Enter insert mode
+    vim.cmd("startinsert!")
+  end
 end
 
 return M
