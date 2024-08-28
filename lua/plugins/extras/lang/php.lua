efe5025dc01497f8bc25c17052083af85736c17e
jourdyGiga <jourdy@gigaming.io>
2024-08-28 21:10:13 +0700

    docs: my php config is deprecated

--- lua/plugins/extras/lang/php.lua
+++ lua/plugins/extras/lang/php.lua
@@ -4,32 +4,34 @@ local printf = require("plugins.util.printf").printf
 
 local php_keymaps = augroup("php_keymaps", {})
 
+-- NOTE: depracated. use lazyvim php instead.
+-- maybe the tree-sitter-blade causes the performance issue?
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    config = function(_, opts)
-      -- NOTE: treesitter blade is not supported yet. so add the parser manually like below and install it.
-      -- see this link https://github.com/EmranMR/tree-sitter-blade/discussions/19#discussion-5400675 for details.
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   config = function(_, opts)
+  --     -- NOTE: treesitter blade is not supported yet. so add the parser manually like below and install it.
+  --     -- see this link https://github.com/EmranMR/tree-sitter-blade/discussions/19#discussion-5400675 for details.
+  --     local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
+  --     parser_config.blade = {
+  --       install_info = {
+  --         url = "https://github.com/EmranMR/tree-sitter-blade",
+  --         files = { "src/parser.c" },
+  --         branch = "main",
+  --       },
+  --       filetype = "blade",
+  --     }
 
-      vim.filetype.add({
-        pattern = {
-          [".*%.blade%.php"] = "blade",
-        },
-      })
+  --     vim.filetype.add({
+  --       pattern = {
+  --         [".*%.blade%.php"] = "blade",
+  --       },
+  --     })
 
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-  },
+  --     require("nvim-treesitter.configs").setup(opts)
+  --   end,
+  -- },
 
   {
     "neovim/nvim-lspconfig",
@@ -77,16 +79,17 @@ return {
   },
 
   -- plugin to help with laravel project. this is way better than nvim version as it doesnt call the command in the notes below that causes timeout. thus rendering the plugin useless.
-  {
-    "noahfrederick/vim-laravel",
-    dependencies = {
-      -- dependencies are needed to run the command from vim-laravel. TODO: add the most usefull command as keymaps above.
-      "tpope/vim-dispatch",
-      "tpope/vim-projectionist",
-      "noahfrederick/vim-composer",
-    },
-    event = "VeryLazy",
-  },
+  -- {
+  --   "noahfrederick/vim-laravel",
+  --   event = "LazyFile",
+  --   dependencies = {
+  --     -- dependencies are needed to run the command from vim-laravel. TODO: add the most usefull command as keymaps above.
+  --     -- NOTE: it uses vim code and causes lspconfig nvim to forced to reload first at startup? causing performance issue.
+  --     "tpope/vim-dispatch",
+  --     "tpope/vim-projectionist",
+  --     "noahfrederick/vim-composer",
+  --   },
+  -- },
 
   -- Ensure PHP tools are installed
   {
