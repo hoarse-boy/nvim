aaee25847527f879d506a13a0ff4e29af896b015
jourdyGiga <jourdy@gigaming.io>
2024-09-04 13:30:45 +0700

    fix: a bug where the dap will show double option that is buggy for golang only

--- lua/plugins/extras/dap/dap.lua
+++ lua/plugins/extras/dap/dap.lua
@@ -1,39 +1,145 @@
-return {
-  -- FIX: using this, the build is successfull. but the json5 is still not found.
-  -- https://github.com/neovim/neovim/issues/21749
-  -- https://github.com/jamylak/nvim-github-codesearch/commit/e82c1b6452c2b890610c586562b233a66c296c00
-  -- {
-  --   "Joakker/lua-json5",
-  --   lazy = false,
-  --   build = "./install.sh",
-  --   config = function()
-  --     require("json5")
-  --   end,
-  -- },
+-- NOTE: a carbon copy from lazyvim with some modification to fix golang double debugger option.
 
+---@param config {args?:string[]|fun():string[]?}
+local function get_args(config)
+  local args = type(config.args) == "function" and (config.args() or {}) or config.args or {}
+  config = vim.deepcopy(config)
+  ---@cast args string[]
+  config.args = function()
+    local new_args = vim.fn.input("Run with args: ", table.concat(args, " ")) --[[@as string]]
+    return vim.split(vim.fn.expand(new_args) --[[@as string]], " ")
+  end
+  return config
+end
+
+return {
   {
     "mfussenegger/nvim-dap",
+    recommended = true,
+    desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+
     dependencies = {
-      -- TODO: find out how to install and compile it https://github.com/Joakker/lua-json5
-      -- FIX: fail to build. find out why
-      -- https://github.com/folke/lazy.nvim/issues/368
-      -- require('dap.ext.vscode').json_decode = require'json5'.parse
-      -- {
-      --   "Joakker/lua-json5",
-      --   build = "./install.sh",
-      -- },
+      "rcarriga/nvim-dap-ui",
+      -- virtual text for the debugger
+      {
+        "theHamsta/nvim-dap-virtual-text",
+        opts = {},
+      },
+      {
+        "Joakker/lua-json5",
+        event = "VeryLazy",
+        build = "./install.sh", -- NOTE: make sure it to installed the latest version of rust, create, and cargo. last time it was failed when building it.
+        config = function()
+          require("json5") -- TODO: disable json lsp or use jsonc lsp if available.
+        end,
+      },
     },
-    opts = function()
-      -- NOTE: enabling nvim to load .vscode/launch.json file for debuging
-      -- add rt_lldb for debuging rust using plugin rust-tools
-      -- require("dap.ext.vscode").load_launchjs(nil, { rt_lldb = { "rust" } }) -- NOTE: disable this as it is already loaded by nvim dap to use vscode dap. if this is enabled it will make the launch.json to more than 1 when selecting dap.
-      -- NOTE: open dap ui again in case the nvim dap existed / terminated to check if there is error log.
-      -- however, developer must log if an error occured to make it easy to fix the issue to start debugging.
+
+    -- stylua: ignore
+    keys = {
+      { "<leader>d",  "",                                                                                   desc = "+debug",                 mode = { "n", "v" } },
+      { "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+      { "<leader>db", function() require("dap").toggle_breakpoint() end,                                    desc = "Toggle Breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end,                                             desc = "Continue" },
+      { "<leader>da", function() require("dap").continue({ before = get_args }) end,                        desc = "Run with Args" },
+      { "<leader>dC", function() require("dap").run_to_cursor() end,                                        desc = "Run to Cursor" },
+      { "<leader>dg", function() require("dap").goto_() end,                                                desc = "Go to Line (No Execute)" },
+      { "<leader>di", function() require("dap").step_into() end,                                            desc = "Step Into" },
+      { "<leader>dj", function() require("dap").down() end,                                                 desc = "Down" },
+      { "<leader>dk", function() require("dap").up() end,                                                   desc = "Up" },
+      { "<leader>dl", function() require("dap").run_last() end,                                             desc = "Run Last" },
+      { "<leader>do", function() require("dap").step_out() end,                                             desc = "Step Out" },
+      { "<leader>dO", function() require("dap").step_over() end,                                            desc = "Step Over" },
+      { "<leader>dp", function() require("dap").pause() end,                                                desc = "Pause" },
+      { "<leader>dr", function() require("dap").repl.toggle() end,                                          desc = "Toggle REPL" },
+      { "<leader>ds", function() require("dap").session() end,                                              desc = "Session" },
+      { "<leader>dt", function() require("dap").terminate() end,                                            desc = "Terminate" },
+      { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                     desc = "Widgets" },
+    },
+
+    config = function()
+      -- load mason-nvim-dap here, after all adapters have been setup
+      if LazyVim.has("mason-nvim-dap.nvim") then
+        require("mason-nvim-dap").setup(LazyVim.opts("mason-nvim-dap.nvim"))
+      end
+
+      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+
+      for name, sign in pairs(LazyVim.config.icons.dap) do
+        sign = type(sign) == "table" and sign or { sign }
+        vim.fn.sign_define("Dap" .. name, { text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] })
+      end
+
+      require("dap.ext.vscode").json_decode = require("json5").parse -- makes the json to have comments.
 
       local printf = require("plugins.util.printf").printf
-      vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", { noremap = true, silent = true, desc= printf("Reset DAP UI Windows") })
-      vim.api.nvim_set_keymap("n", "<leader>dR", ":lua require('dap').repl.toggle()<CR>", { noremap = true, silent = true, desc= printf("Toggle REPL") })
-      vim.api.nvim_set_keymap("n", "<leader>du", ":lua require('dapui').toggle({})<CR>", { noremap = true, silent = true, desc= printf("Toggle DAP UI") })
+      vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", { noremap = true, silent = true, desc = printf("Reset DAP UI Windows") })
+      vim.api.nvim_set_keymap("n", "<leader>dR", ":lua require('dap').repl.toggle()<CR>", { noremap = true, silent = true, desc = printf("Toggle REPL") })
+      vim.api.nvim_set_keymap("n", "<leader>du", ":lua require('dapui').toggle({})<CR>", { noremap = true, silent = true, desc = printf("Toggle DAP UI") })
+
+      -- NOTE: don't remove this comment.
+      -- below is the code from lazyvim that makes vscode debugger to have double debugger option.
+      -- and the option is not working because the env is not loaded. the second option works fine.
+      -- setup dap config by VsCode launch.json file
+      -- local vscode = require("dap.ext.vscode")
+      -- local json = require("plenary.json")
+      -- vscode.json_decode = function(str)
+      --   return vim.json.decode(json.json_strip_comments(str))
+      -- end
+      -- Extends dap.configurations with entries read from .vscode/launch.json
+      -- if vim.fn.filereadable(".vscode/launch.json") then
+      -- vscode.load_launchjs()
+      -- end
     end,
+  },
+
+  -- fancy UI for the debugger
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = { "nvim-neotest/nvim-nio" },
+    -- stylua: ignore
+    keys = {
+      { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
+      { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
+    },
+    opts = {},
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close({})
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close({})
+      end
+    end,
+  },
+
+  -- mason.nvim integration
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = "mason.nvim",
+    cmd = { "DapInstall", "DapUninstall" },
+    opts = {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_installation = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      handlers = {},
+
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      ensure_installed = {
+        -- Update this to ensure that you have the debuggers for the langs you want
+      },
+    },
+    -- mason-nvim-dap is loaded when nvim-dap loads
+    config = function() end,
   },
 }
